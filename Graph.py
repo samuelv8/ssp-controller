211557785f4d552f1bf13b5534d8a9c0a933c00f
samuelv8 <78799492+samuelv8@users.noreply.github.com>
2021-07-12 09:59:47 -0300

    Updated Graph class adjacency list

--- Graph.py
+++ Graph.py
@@ -9,16 +9,19 @@ class Graph:
         """
         self.n_vertices = 0
         self.n_edges = 0
-        self.adj_list = {}  # key: vertex, value: list of lists with adjacent vertices and related weights
+        self.adj_list = {}  # key: vertex, value: dict with adjacent vertices and related weights
 
     def add_vertex(self, v):
-        self.adj_list[v] = []
+        self.adj_list[v] = {}
         self.n_vertices += 1
 
     def add_edge(self, u, v, weight):
-        if [v, weight] not in self.adj_list[u]:
-            self.adj_list[u].append([v, weight])
+        # only adds an (u,v) edge if there's no edge from u to v or if it will add a lower weight edge
+        if v not in self.adj_list[u]:
+            self.adj_list[u][v] = weight
             self.n_edges += 1
+        elif self.adj_list[u][v] > weight:
+            self.adj_list[u][v] = weight
 
         if v not in self.adj_list:
             self.n_vertices += 1
